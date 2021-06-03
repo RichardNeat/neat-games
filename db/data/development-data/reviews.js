7548f0dfa83ae557a407fc14d8722ab0ae9d896e
P-Copley <pkcopley@gmail.com>
2021-06-03 10:04:47 +0100

    replace unix timestamps -> Dates

--- db/data/development-data/reviews.js
+++ db/data/development-data/reviews.js
@@ -8,7 +8,7 @@ module.exports = [
     review_body:
       "You could sum up Agricola with the simple phrase 'Farmyeard Fun' but the mechanics and game play add so much more than that. You'll find yourself torn between breeding pigs, or sowing crops. Its joyeous and rewarding and it makes you think of time spent outside, which is much harder to do these days!",
     category: 'strategy',
-    created_at: 1610964020514,
+    created_at: new Date(1610964020514),
     votes: 1,
   },
   {
@@ -20,7 +20,7 @@ module.exports = [
     review_body:
       "Few games are equiped to fill a player with such a defined sense of mild-peril, but a friendly game of Jenga will turn the mustn't-make-it-fall anxiety all the way up to 11! Fiddly fun for all the family, this game needs little explaination. Whether you're a player who chooses to play it safe, or one who lives life on the edge, eventually the removal of blocks will destabilise the tower and all your Jenga dreams come tumbling down.",
     category: 'dexterity',
-    created_at: 1610964101251,
+    created_at: new Date(1610964101251),
     votes: 5,
   },
   {
@@ -32,7 +32,7 @@ module.exports = [
     review_body:
       'Try to trick your friends. If you find yourself being dealt the Chamelean card then the aim of the game is simple; blend in... Meanwhile the other players aim to be as vague as they can to not give the game away ',
     category: 'hidden-roles',
-    created_at: 1610964102151,
+    created_at: new Date(1610964102151),
     votes: 5,
   },
   {
@@ -43,7 +43,7 @@ module.exports = [
       'https://images.pexels.com/photos/5350049/pexels-photo-5350049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     review_body: "We couldn't find the werewolf!",
     category: 'hidden-roles',
-    created_at: 1610964101251,
+    created_at: new Date(1610964101251),
     votes: 5,
   },
   {
@@ -53,7 +53,7 @@ module.exports = [
     review_body:
       "Ever wish you could try your hand at mixing potions? Quacks of Quedlinburg will have you mixing up a homebrew like no other. Each player buys different ingredients (chips) that are drawn at random to reach the most points, but watch out, you'd better not let your cauldrom explode.",
     category: 'push-your-luck',
-    created_at: 1610964101251,
+    created_at: new Date(1610964101251),
     votes: 10,
   },
   {
@@ -65,7 +65,7 @@ module.exports = [
     review_body:
       'Cold rain pours on the faces of your team of cyclists, you pulled to the front of the pack early and now your taking on exhaustion cards like there is not tomorrow, you think there are about 2 hands left until you cross the finish line, will you draw enough from your deck to cross before the other team shoot passed? Flamee Rouge is a Racing deck management game where you carefully manage your deck in order to cross the line before your opponents, cyclist can fall slyly behind front runners in their slipstreams to save precious energy for the prefect moment to burst into the lead ',
     category: 'deck-building',
-    created_at: 1610964101251,
+    created_at: new Date(1610964101251),
     votes: 10,
   },
   {
@@ -75,7 +75,7 @@ module.exports = [
     review_body:
       "If you've ever wanted to accuse your siblings, cousins or friends of being part of a plot to murder everyone whilst secretly choosing which one of them should get the chop next - this is the boardgame for you. Buyer beware: once you gain a reputation for being able to lie with a stone face about being the secret killer you may never lose it.",
     category: 'hidden-roles',
-    created_at: 1610964101251,
+    created_at: new Date(1610964101251),
     votes: 8,
   },
   {
@@ -85,7 +85,7 @@ module.exports = [
     review_body:
       'Spend 30 minutes just setting up all of the boards (!) meeple and decks, just to forget how to play. Scythe can be a lengthy game but really packs a punch if you put the time in. With beautiful artwork, countless scenarios and clever game mechanics, this board game is a must for any board game fanatic; just make sure you explain ALL the rules before you start playing with first timers or you may find they bring it up again and again.',
     category: 'engine-building',
-    created_at: 1611311824839,
+    created_at: new Date(1611311824839),
     votes: 100,
   },
   {
@@ -95,7 +95,7 @@ module.exports = [
     review_body:
       'You have stumbled across an uncharted island rich in natural resources, but you are not alone; other adventurers have come ashore too, and the race to settle the island of Catan has begun! Whether you exert military force, build a road to rival the Great Wall, trade goods with ships from the outside world, or some combination of all three, the aim is the same: to dominate the island. Will you prevail? Proceed strategically, trade wisely, and may the odds be in favour.',
     category: 'strategy',
-    created_at: 788918400,
+    created_at: new Date(788918400),
     votes: 16,
   },
   {
@@ -107,7 +107,7 @@ module.exports = [
     review_body:
       'Consequat velit occaecat voluptate do. Dolor pariatur fugiat sint et proident ex do consequat est. Nisi minim laboris mollit cupidatat et adipisicing laborum do. Sint sit tempor officia pariatur duis ullamco labore ipsum nisi voluptate nulla eu veniam. Et do ad id dolore id cillum non non culpa. Cillum mollit dolor dolore excepteur aliquip. Cillum aliquip quis aute enim anim ex laborum officia. Aliqua magna elit reprehenderit Lorem elit non laboris irure qui aliquip ad proident. Qui enim mollit Lorem labore eiusmod',
     category: 'dexterity',
-    created_at: 1611315350936,
+    created_at: new Date(1611315350936),
     votes: 7,
   },
   {
@@ -117,7 +117,7 @@ module.exports = [
     review_body:
       'Labore occaecat sunt qui commodo anim anim aliqua adipisicing aliquip fugiat. Ad in ipsum incididunt esse amet deserunt aliqua exercitation occaecat nostrud irure labore ipsum. Culpa tempor non voluptate reprehenderit deserunt pariatur cupidatat aliqua adipisicing. Nostrud labore dolor fugiat sint consequat excepteur dolore irure eu. Anim ex adipisicing magna deserunt enim fugiat do nulla officia sint. Ex tempor ut aliquip exercitation eiusmod. Excepteur deserunt officia voluptate sunt aliqua esse deserunt velit. In id non proident veniam ipsum id in consequat duis ipsum et incididunt. Qui cupidatat ea deserunt magna proident nisi nulla eiusmod aliquip magna deserunt fugiat fugiat incididunt. Laboris nisi velit mollit ullamco deserunt eiusmod deserunt ea dolore veniam.',
     category: 'engine-building',
-    created_at: 1610010368077,
+    created_at: new Date(1610010368077),
     votes: 5,
   },
   {
@@ -129,7 +129,7 @@ module.exports = [
     review_body:
       'Fugiat fugiat enim officia laborum quis. Aliquip laboris non nulla nostrud magna exercitation in ullamco aute laborum cillum nisi sint. Culpa excepteur aute cillum minim magna fugiat culpa adipisicing eiusmod laborum ipsum fugiat quis. Mollit consectetur amet sunt ex amet tempor magna consequat dolore cillum adipisicing. Proident est sunt amet ipsum magna proident fugiat deserunt mollit officia magna ea pariatur. Ullamco proident in nostrud pariatur. Minim consequat pariatur id pariatur adipisicing.',
     category: 'roll-and-write',
-    created_at: 1600010368077,
+    created_at: new Date(1600010368077),
     votes: 8,
   },
   {
@@ -141,7 +141,7 @@ module.exports = [
     review_body:
       "Don't underestimate the tension and supsense that can be brought on with a round of Kerplunk! You'll feel the rush and thrill of not disturbing the stack of marbles, and probably utter curse words when you draw the wrong straw. Fanily friendly, and not just for kids! ",
     category: 'dexterity',
-    created_at: 1611573414963,
+    created_at: new Date(1611573414963),
     votes: 9,
   },
   {
@@ -151,7 +151,7 @@ module.exports = [
     review_body:
       'Nostrud anim cupidatat incididunt officia cupidatat magna. Cillum commodo voluptate laboris id incididunt esse elit ipsum consectetur non elit elit magna. Aliquip sint amet eiusmod magna. Fugiat non ut ex eiusmod elit. Esse anim irure laborum aute ut ad reprehenderit. Veniam laboris dolore mollit mollit in. Cillum in aliquip adipisicing ipsum et dolor veniam qui ut ullamco aliquip in. Dolor fugiat elit laborum elit cupidatat aute qui nostrud. Duis incididunt ea nostrud minim consequat. Reprehenderit mollit cupidatat do culpa aliqua culpa mollit minim eiusmod. Deserunt occaecat ipsum ex ut pariatur eu veniam cillum nulla ex nostrud. Do nostrud amet duis proident nostrud eiusmod occaecat reprehenderit. Quis et cupidatat tempor qui dolor id veniam in sunt ipsum eiusmod. Sint tempor commodo consectetur mollit proident culpa nulla est tempor ullamco tempor aliquip laboris.',
     category: 'hidden-roles',
-    created_at: 1612524446563,
+    created_at: new Date(1612524446563),
     votes: 3,
   },
   {
@@ -163,7 +163,7 @@ module.exports = [
     review_body:
       "You know; the one that looks a lot like Scrabble, and plays a lot like Scrabble, but you have to push the letter tiles out of cardboard becasue that makes it more \"fun\"...  If you're a fan of words you'll love this game regardless, but if you're a hard core word nerd you probably have the original anyway.",
     category: 'strategy',
-    created_at: 1611315305936,
+    created_at: new Date(1611315305936),
     votes: 1,
   },
   {
@@ -175,7 +175,7 @@ module.exports = [
     review_body:
       'Choo-chooing onto game tables and zooming its way to becoming a modern classic. Ticket To Ride is rich with trains, tickets and tactics. Players gather train cards to enable then to build routes across a North American map, joining big cities and trying not to get blocked by their opponent ',
     category: 'deck-building',
-    created_at: 1611313505936,
+    created_at: new Date(1611313505936),
     votes: 1,
   },
   {
@@ -185,7 +185,7 @@ module.exports = [
     review_body:
       "Loosely based around the phases of the moon, this tile placing game is rich in strategy anf makes great use of non-standard turn taking. Nova Luna was one of my favourite nominations for the prestigious Kennerspiel des Jahres in 2020. Sadly it didn't take home the title, but its an intruguing abstract game, certainly one to add to your game shelf.",
     category: 'strategy',
-    created_at: 788198400,
+    created_at: new Date(788198400),
     votes: 6,
   },
   {
@@ -195,7 +195,7 @@ module.exports = [
     review_body:
       'This game reminds me of the stress-free environment described in a song sung by a crab in the famous film about a mermaid. Plenty of coral collecting, and reef building to keep you entertained ',
     category: 'strategy',
-    created_at: 788198400,
+    created_at: new Date(788198400),
     votes: 6,
   },
   //https://images.pexels.com/photos/207924/pexels-photo-207924.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260 - Ludo
@@ -210,7 +210,7 @@ module.exports = [
     review_body:
       'Fugiat fugiat enim officia laborum quis. Aliquip laboris non nulla nostrud magna exercitation in ullamco aute laborum cillum nisi sint. Culpa excepteur aute cillum minim magna fugiat culpa adipisicing eiusmod laborum ipsum fugiat quis. Mollit consectetur amet sunt ex amet tempor magna consequat dolore cillum adipisicing. Proident est sunt amet ipsum magna proident fugiat deserunt mollit officia magna ea pariatur. Ullamco proident in nostrud pariatur. Minim consequat pariatur id pariatur adipisicing.',
     category: 'roll-and-write',
-    created_at: 1600010368077,
+    created_at: new Date(1600010368077),
     votes: 8,
   },
   {
@@ -222,7 +222,7 @@ module.exports = [
     review_body:
       'This household classic needs no introduction. Monopoly has been causeing family fallouts for close to 90 years. With numerous special editions and no doubt more still to come almost everyone has played this game, but has anyone ever finished it?',
     category: 'strategy',
-    created_at: 1600010060877,
+    created_at: new Date(1600010060877),
     votes: 3,
   },
   {
@@ -234,7 +234,7 @@ module.exports = [
     review_body:
       "The title translates to \"that's so clever\" and it won't take you long to realise why. Ganz Schon Clever is a really engagind roll-and-write where you'll try to collect score points from coloured dice. Sounds simple, but the real strategy of this game lies in how you collect the bonuses.",
     category: 'roll-and-write',
-    created_at: 1600010368077,
+    created_at: new Date(1600010368077),
     votes: 8,
   },
   {
@@ -246,7 +246,7 @@ module.exports = [
     review_body:
       'Often thought of as the ultimate roll-and-write game, You\'ll find it hard to overlook Yahtzee\'s appeal. Yahtzee is quick to teach and quick to play. Think of it as an essential "palate-cleanser" game to have in your store cupboard for those times when chunkier games leave a bitter taste in players mouths.',
     category: 'roll-and-write',
-    created_at: 1600010360077,
+    created_at: new Date(1600010360077),
     votes: 18,
   },
   {
@@ -258,7 +258,7 @@ module.exports = [
     review_body:
       'Escape the Dark Castle is a cooperative board game than will bring back feelings of eighties nostalgia. Released in 2017, its already well on its way to being a cult classic. Filled with dark traps, dungeons and monsterous foes the aim of the game is simple, escape!',
     category: 'push-your-luck',
-    created_at: 1610964545410,
+    created_at: new Date(1610964545410),
     votes: 18,
   },
   {
@@ -270,7 +270,7 @@ module.exports = [
     review_body:
       "Argueably one of the best things to come out of 2020, and new addition to what will hopefully become more than a two part series. Escape the Dark Sector takes everything about Escape the Dark Castle that made it wonderful, and then adds guns! Thrust into the future by the imersive chapter cards, your mission remains the same, (thats right, escape!) but the challenges you'll face feel a whole lot more grown up. Cooperative and atmospheric, Escape the Dark Sector brings more variety and a bigger challenge to players. Don't underestimate the dabger of running out of ammo.",
     category: 'push-your-luck',
-    created_at: 1610964545610,
+    created_at: new Date(1610964545610),
     votes: 11,
   },
 ];
--- db/data/test-data/reviews.js
+++ db/data/test-data/reviews.js
@@ -5,7 +5,7 @@ module.exports = [
     owner: 'mallionaire',
     review_body: 'Farmyard fun!',
     category: 'euro game',
-    created_at: 1610964020514,
+    created_at: new Date(1610964020514),
     votes: 1,
     review_img_url:
       'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
@@ -16,7 +16,7 @@ module.exports = [
     owner: 'philippaclaire9',
     review_body: 'Fiddly fun for all the family',
     category: 'dexterity',
-    created_at: 1610964101251,
+    created_at: new Date(1610964101251),
     votes: 5,
     review_img_url:
       'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
@@ -27,7 +27,7 @@ module.exports = [
     owner: 'bainesface',
     review_body: "We couldn't find the werewolf!",
     category: 'social deduction',
-    created_at: 1610964101251,
+    created_at: new Date(1610964101251),
     votes: 5,
     review_img_url:
       'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
@@ -41,7 +41,7 @@ module.exports = [
     review_body:
       'Consequat velit occaecat voluptate do. Dolor pariatur fugiat sint et proident ex do consequat est. Nisi minim laboris mollit cupidatat et adipisicing laborum do. Sint sit tempor officia pariatur duis ullamco labore ipsum nisi voluptate nulla eu veniam. Et do ad id dolore id cillum non non culpa. Cillum mollit dolor dolore excepteur aliquip. Cillum aliquip quis aute enim anim ex laborum officia. Aliqua magna elit reprehenderit Lorem elit non laboris irure qui aliquip ad proident. Qui enim mollit Lorem labore eiusmod',
     category: 'social deduction',
-    created_at: 1611315350936,
+    created_at: new Date(1611315350936),
     votes: 7,
   },
   {
@@ -51,7 +51,7 @@ module.exports = [
     review_body:
       'Labore occaecat sunt qui commodo anim anim aliqua adipisicing aliquip fugiat. Ad in ipsum incididunt esse amet deserunt aliqua exercitation occaecat nostrud irure labore ipsum. Culpa tempor non voluptate reprehenderit deserunt pariatur cupidatat aliqua adipisicing. Nostrud labore dolor fugiat sint consequat excepteur dolore irure eu. Anim ex adipisicing magna deserunt enim fugiat do nulla officia sint. Ex tempor ut aliquip exercitation eiusmod. Excepteur deserunt officia voluptate sunt aliqua esse deserunt velit. In id non proident veniam ipsum id in consequat duis ipsum et incididunt. Qui cupidatat ea deserunt magna proident nisi nulla eiusmod aliquip magna deserunt fugiat fugiat incididunt. Laboris nisi velit mollit ullamco deserunt eiusmod deserunt ea dolore veniam.',
     category: 'social deduction',
-    created_at: 1610010368077,
+    created_at: new Date(1610010368077),
     votes: 5,
   },
   {
@@ -63,7 +63,7 @@ module.exports = [
     review_body:
       'Fugiat fugiat enim officia laborum quis. Aliquip laboris non nulla nostrud magna exercitation in ullamco aute laborum cillum nisi sint. Culpa excepteur aute cillum minim magna fugiat culpa adipisicing eiusmod laborum ipsum fugiat quis. Mollit consectetur amet sunt ex amet tempor magna consequat dolore cillum adipisicing. Proident est sunt amet ipsum magna proident fugiat deserunt mollit officia magna ea pariatur. Ullamco proident in nostrud pariatur. Minim consequat pariatur id pariatur adipisicing.',
     category: 'social deduction',
-    created_at: 1600010368077,
+    created_at: new Date(1600010368077),
     votes: 8,
   },
   {
@@ -75,7 +75,7 @@ module.exports = [
     review_body:
       'Consectetur incididunt aliquip sunt officia. Magna ex nulla consectetur laboris incididunt ea non qui. Enim id eiusmod irure dolor ipsum in tempor consequat amet ullamco. Occaecat fugiat sint fugiat mollit consequat pariatur consequat non exercitation dolore. Labore occaecat in magna commodo anim enim eiusmod eu pariatur ad duis magna. Voluptate ad et dolore ullamco anim sunt do. Qui exercitation tempor in in minim ullamco fugiat ipsum. Duis irure voluptate cupidatat do id mollit veniam culpa. Velit deserunt exercitation amet laborum nostrud dolore in occaecat minim amet nostrud sunt in. Veniam ut aliqua incididunt commodo sint in anim duis id commodo voluptate sit quis.',
     category: 'social deduction',
-    created_at: 1611573414963,
+    created_at: new Date(1611573414963),
     votes: 9,
   },
   {
@@ -86,7 +86,7 @@ module.exports = [
       'https://images.pexels.com/photos/5350049/pexels-photo-5350049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     review_body: "We couldn't find the werewolf!",
     category: 'social deduction',
-    created_at: 1610964101251,
+    created_at: new Date(1610964101251),
     votes: 5,
   },
   {
@@ -96,7 +96,7 @@ module.exports = [
     review_body:
       "Ever wish you could try your hand at mixing potions? Quacks of Quedlinburg will have you mixing up a homebrew like no other. Each player buys different ingredients (chips) that are drawn at random to reach the most points, but watch out, you'd better not let your cauldrom explode.",
     category: 'social deduction',
-    created_at: 1610964101251,
+    created_at: new Date(1610964101251),
     votes: 10,
   },
   {
@@ -108,7 +108,7 @@ module.exports = [
     review_body:
       'Cold rain pours on the faces of your team of cyclists, you pulled to the front of the pack early and now your taking on exhaustion cards like there is not tomorrow, you think there are about 2 hands left until you cross the finish line, will you draw enough from your deck to cross before the other team shoot passed? Flamee Rouge is a Racing deck management game where you carefully manage your deck in order to cross the line before your opponents, cyclist can fall slyly behind front runners in their slipstreams to save precious energy for the prefect moment to burst into the lead ',
     category: 'social deduction',
-    created_at: 1610964101251,
+    created_at: new Date(1610964101251),
     votes: 10,
   },
   {
@@ -118,7 +118,7 @@ module.exports = [
     review_body:
       "If you've ever wanted to accuse your siblings, cousins or friends of being part of a plot to murder everyone whilst secretly choosing which one of them should get the chop next - this is the boardgame for you. Buyer beware: once you gain a reputation for being able to lie with a stone face about being the secret killer you may never lose it.",
     category: 'social deduction',
-    created_at: 1610964101251,
+    created_at: new Date(1610964101251),
     votes: 8,
   },
   {
@@ -128,7 +128,7 @@ module.exports = [
     review_body:
       'Spend 30 minutes just setting up all of the boards (!) meeple and decks, just to forget how to play. Scythe can be a lengthy game but really packs a punch if you put the time in. With beautiful artwork, countless scenarios and clever game mechanics, this board game is a must for any board game fanatic; just make sure you explain ALL the rules before you start playing with first timers or you may find they bring it up again and again.',
     category: 'social deduction',
-    created_at: 1611311824839,
+    created_at: new Date(1611311824839),
     votes: 100,
   },
   {
@@ -138,7 +138,7 @@ module.exports = [
     review_body:
       'You have stumbled across an uncharted island rich in natural resources, but you are not alone; other adventurers have come ashore too, and the race to settle the island of Catan has begun! Whether you exert military force, build a road to rival the Great Wall, trade goods with ships from the outside world, or some combination of all three, the aim is the same: to dominate the island. Will you prevail? Proceed strategically, trade wisely, and may the odds be in favour.',
     category: 'social deduction',
-    created_at: 788918400,
+    created_at: new Date(788918400),
     votes: 16,
   },
 ];
