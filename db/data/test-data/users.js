f22d1e982bfecd39e46f6523bda2de6ff19f34ad
dav3rid <davecodingstuff@gmail.com>
2021-06-15 09:44:10 +0100

    remove trailing commas from users dev + test

--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -3,22 +3,22 @@ module.exports = [
     username: 'mallionaire',
     name: 'haz',
     avatar_url:
-      'https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg',
+      'https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg'
   },
   {
     username: 'philippaclaire9',
     name: 'philippa',
-    avatar_url: 'https://avatars2.githubusercontent.com/u/24604688?s=460&v=4',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/24604688?s=460&v=4'
   },
   {
     username: 'bainesface',
     name: 'sarah',
-    avatar_url: 'https://avatars2.githubusercontent.com/u/24394918?s=400&v=4',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/24394918?s=400&v=4'
   },
   {
     username: 'dav3rid',
     name: 'dave',
     avatar_url:
-      'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
-  },
+      'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png'
+  }
 ];
