3eebfc9a53f2dbf9aa7d54bc98ab1c2ed10538d5
dav3rid <davecodingstuff@gmail.com>
2021-06-15 09:50:21 +0100

    add review_img_url property to reviews test data

--- db/data/test-data/reviews.js
+++ db/data/test-data/reviews.js
@@ -3,34 +3,34 @@ module.exports = [
     title: 'Agricola',
     designer: 'Uwe Rosenberg',
     owner: 'mallionaire',
+    review_img_url:
+      'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
     review_body: 'Farmyard fun!',
     category: 'euro game',
     created_at: new Date(1610964020514),
-    votes: 1,
-    review_img_url:
-      'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
+    votes: 1
   },
   {
     title: 'Jenga',
     designer: 'Leslie Scott',
     owner: 'philippaclaire9',
+    review_img_url:
+      'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
     review_body: 'Fiddly fun for all the family',
     category: 'dexterity',
     created_at: new Date(1610964101251),
-    votes: 5,
-    review_img_url:
-      'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
+    votes: 5
   },
   {
     title: 'Ultimate Werewolf',
     designer: 'Akihisa Okui',
     owner: 'bainesface',
+    review_img_url:
+      'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
     review_body: "We couldn't find the werewolf!",
     category: 'social deduction',
     created_at: new Date(1610964101251),
-    votes: 5,
-    review_img_url:
-      'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
+    votes: 5
   },
   {
     title: 'Dolor reprehenderit',
@@ -42,17 +42,19 @@ module.exports = [
       'Consequat velit occaecat voluptate do. Dolor pariatur fugiat sint et proident ex do consequat est. Nisi minim laboris mollit cupidatat et adipisicing laborum do. Sint sit tempor officia pariatur duis ullamco labore ipsum nisi voluptate nulla eu veniam. Et do ad id dolore id cillum non non culpa. Cillum mollit dolor dolore excepteur aliquip. Cillum aliquip quis aute enim anim ex laborum officia. Aliqua magna elit reprehenderit Lorem elit non laboris irure qui aliquip ad proident. Qui enim mollit Lorem labore eiusmod',
     category: 'social deduction',
     created_at: new Date(1611315350936),
-    votes: 7,
+    votes: 7
   },
   {
     title: 'Proident tempor et.',
     designer: 'Seymour Buttz',
     owner: 'mallionaire',
+    review_img_url:
+      'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
     review_body:
       'Labore occaecat sunt qui commodo anim anim aliqua adipisicing aliquip fugiat. Ad in ipsum incididunt esse amet deserunt aliqua exercitation occaecat nostrud irure labore ipsum. Culpa tempor non voluptate reprehenderit deserunt pariatur cupidatat aliqua adipisicing. Nostrud labore dolor fugiat sint consequat excepteur dolore irure eu. Anim ex adipisicing magna deserunt enim fugiat do nulla officia sint. Ex tempor ut aliquip exercitation eiusmod. Excepteur deserunt officia voluptate sunt aliqua esse deserunt velit. In id non proident veniam ipsum id in consequat duis ipsum et incididunt. Qui cupidatat ea deserunt magna proident nisi nulla eiusmod aliquip magna deserunt fugiat fugiat incididunt. Laboris nisi velit mollit ullamco deserunt eiusmod deserunt ea dolore veniam.',
     category: 'social deduction',
     created_at: new Date(1610010368077),
-    votes: 5,
+    votes: 5
   },
   {
     title: 'Occaecat consequat officia in quis commodo.',
@@ -64,7 +66,7 @@ module.exports = [
       'Fugiat fugiat enim officia laborum quis. Aliquip laboris non nulla nostrud magna exercitation in ullamco aute laborum cillum nisi sint. Culpa excepteur aute cillum minim magna fugiat culpa adipisicing eiusmod laborum ipsum fugiat quis. Mollit consectetur amet sunt ex amet tempor magna consequat dolore cillum adipisicing. Proident est sunt amet ipsum magna proident fugiat deserunt mollit officia magna ea pariatur. Ullamco proident in nostrud pariatur. Minim consequat pariatur id pariatur adipisicing.',
     category: 'social deduction',
     created_at: new Date(1600010368077),
-    votes: 8,
+    votes: 8
   },
   {
     title: 'Mollit elit qui incididunt veniam occaecat cupidatat',
@@ -76,7 +78,7 @@ module.exports = [
       'Consectetur incididunt aliquip sunt officia. Magna ex nulla consectetur laboris incididunt ea non qui. Enim id eiusmod irure dolor ipsum in tempor consequat amet ullamco. Occaecat fugiat sint fugiat mollit consequat pariatur consequat non exercitation dolore. Labore occaecat in magna commodo anim enim eiusmod eu pariatur ad duis magna. Voluptate ad et dolore ullamco anim sunt do. Qui exercitation tempor in in minim ullamco fugiat ipsum. Duis irure voluptate cupidatat do id mollit veniam culpa. Velit deserunt exercitation amet laborum nostrud dolore in occaecat minim amet nostrud sunt in. Veniam ut aliqua incididunt commodo sint in anim duis id commodo voluptate sit quis.',
     category: 'social deduction',
     created_at: new Date(1611573414963),
-    votes: 9,
+    votes: 9
   },
   {
     title: 'One Night Ultimate Werewolf',
@@ -87,17 +89,19 @@ module.exports = [
     review_body: "We couldn't find the werewolf!",
     category: 'social deduction',
     created_at: new Date(1610964101251),
-    votes: 5,
+    votes: 5
   },
   {
     title: 'A truly Quacking Game; Quacks of Quedlinburg',
     designer: 'Wolfgang Warsch',
     owner: 'mallionaire',
+    review_img_url:
+      'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
     review_body:
       "Ever wish you could try your hand at mixing potions? Quacks of Quedlinburg will have you mixing up a homebrew like no other. Each player buys different ingredients (chips) that are drawn at random to reach the most points, but watch out, you'd better not let your cauldrom explode.",
     category: 'social deduction',
     created_at: new Date(1610964101251),
-    votes: 10,
+    votes: 10
   },
   {
     title: 'Build you own tour de Yorkshire',
@@ -109,36 +113,42 @@ module.exports = [
       'Cold rain pours on the faces of your team of cyclists, you pulled to the front of the pack early and now your taking on exhaustion cards like there is not tomorrow, you think there are about 2 hands left until you cross the finish line, will you draw enough from your deck to cross before the other team shoot passed? Flamee Rouge is a Racing deck management game where you carefully manage your deck in order to cross the line before your opponents, cyclist can fall slyly behind front runners in their slipstreams to save precious energy for the prefect moment to burst into the lead ',
     category: 'social deduction',
     created_at: new Date(1610964101251),
-    votes: 10,
+    votes: 10
   },
   {
     title: "That's just what an evil person would say!",
     designer: 'Fiona Lohoar',
     owner: 'mallionaire',
+    review_img_url:
+      'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
     review_body:
       "If you've ever wanted to accuse your siblings, cousins or friends of being part of a plot to murder everyone whilst secretly choosing which one of them should get the chop next - this is the boardgame for you. Buyer beware: once you gain a reputation for being able to lie with a stone face about being the secret killer you may never lose it.",
     category: 'social deduction',
     created_at: new Date(1610964101251),
-    votes: 8,
+    votes: 8
   },
   {
     title: "Scythe; you're gonna need a bigger table!",
     designer: 'Jamey Stegmaier',
     owner: 'mallionaire',
+    review_img_url:
+      'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
     review_body:
       'Spend 30 minutes just setting up all of the boards (!) meeple and decks, just to forget how to play. Scythe can be a lengthy game but really packs a punch if you put the time in. With beautiful artwork, countless scenarios and clever game mechanics, this board game is a must for any board game fanatic; just make sure you explain ALL the rules before you start playing with first timers or you may find they bring it up again and again.',
     category: 'social deduction',
     created_at: new Date(1611311824839),
-    votes: 100,
+    votes: 100
   },
   {
     title: "Settlers of Catan: Don't Settle For Less",
     designer: 'Klaus Teuber',
     owner: 'mallionaire',
+    review_img_url:
+      'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
     review_body:
       'You have stumbled across an uncharted island rich in natural resources, but you are not alone; other adventurers have come ashore too, and the race to settle the island of Catan has begun! Whether you exert military force, build a road to rival the Great Wall, trade goods with ships from the outside world, or some combination of all three, the aim is the same: to dominate the island. Will you prevail? Proceed strategically, trade wisely, and may the odds be in favour.',
     category: 'social deduction',
     created_at: new Date(788918400),
-    votes: 16,
-  },
+    votes: 16
+  }
 ];
