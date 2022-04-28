e84d046b9c159e8d4ba0d6b12d850ac4279c892b
bainesface <bainesface@gmail.com>
2022-04-28 10:18:28 +0100

    complete seeding

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,4 +1,10 @@
 const db = require("../connection");
+const format = require("pg-format");
+const {
+	convertTimestampToDate,
+	createRef,
+	formatComments,
+} = require("./utils");
 
 const seed = async (data) => {
 	const { categoryData, commentData, reviewData, userData } = data;
@@ -44,6 +50,80 @@ const seed = async (data) => {
     votes INT DEFAULT 0 NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
   );`);
+
+	const insertCategoriesQueryStr = format(
+		"INSERT INTO categories (slug, description) VALUES %L RETURNING *;",
+		categoryData.map(({ slug, description }) => [slug, description])
+	);
+
+	const categoriesPromise = db
+		.query(insertCategoriesQueryStr)
+		.then((result) => result.rows);
+
+	const insertUsersQueryStr = format(
+		"INSERT INTO users ( username, name, avatar_url) VALUES %L RETURNING *;",
+		userData.map(({ username, name, avatar_url }) => [
+			username,
+			name,
+			avatar_url,
+		])
+	);
+	const usersPromise = db
+		.query(insertUsersQueryStr)
+		.then((result) => result.rows);
+
+	await Promise.all([categoriesPromise, usersPromise]);
+
+	const formattedReviewData = reviewData.map(convertTimestampToDate);
+
+	const insertReviewsQueryStr = format(
+		"INSERT INTO reviews (title, category, designer, owner, review_body, review_img_url, created_at, votes) VALUES %L RETURNING *;",
+		formattedReviewData.map(
+			({
+				title,
+				category,
+				designer,
+				owner,
+				review_body,
+				review_img_url,
+				created_at,
+				votes,
+			}) => [
+				title,
+				category,
+				designer,
+				owner,
+				review_body,
+				review_img_url,
+				created_at,
+				votes,
+			]
+		)
+	);
+
+	const reviewRows = await db
+		.query(insertReviewsQueryStr)
+		.then((result) => result.rows);
+
+	const reviewIdLookup = createRef(reviewRows, "title", "review_id");
+	const formattedCommentData = formatComments(commentData, reviewIdLookup);
+
+	const insertCommentsQueryStr = format(
+		"INSERT INTO comments (body, author, review_id, votes, created_at) VALUES %L RETURNING *;",
+		formattedCommentData.map(
+			({ body, author, review_id, votes = 0, created_at }) => [
+				body,
+				author,
+				review_id,
+				votes,
+				created_at,
+			]
+		)
+	);
+
+	return db.query(insertCommentsQueryStr).then((result) => {
+		return result.rows;
+	});
 };
 
 module.exports = seed;
